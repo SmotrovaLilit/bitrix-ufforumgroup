9ec98c27c37d881f40e0cf447fb2fc20b57645ac
lilit <lilit2990@gmail.com>
2017-09-16 00:32:15 +0300

    автозагрузчик классов модуля, правки

--- include.php
+++ include.php
@@ -5,7 +5,8 @@ IncludeModuleLangFile(__FILE__);
 CModule::AddAutoloadClasses(
     'smotrovalilit.ufforumgroup',
     [
-        'LS\UFTypeForumGroup\General\CUserTypeForumGroup' => 'classes/general/CUserTypeForumGroup.php'
+        'LS\UFTypeForumGroup\General\CUserTypeForumGroup' => 'classes/general/CUserTypeForumGroup.php',
+        'LS\UFTypeForumGroup\General\CForumGroupEnum' => 'classes/general/CForumGroupEnum.php'
     ]
 );
 
--- install/version.php
+++ install/version.php
@@ -1,6 +1,6 @@
 <?php
 $arModuleVersion = array(
-	"VERSION" => "1.0.0",
-	"VERSION_DATE" => "2017-09-15 13:00:00"
+	"VERSION" => "1.0.3",
+	"VERSION_DATE" => "2017-09-16 00:30:00"
 );
 ?>
